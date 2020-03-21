b35cc9482203e730e1d0fdbba306775593d3d3bc
will <will@will>
2020-03-21 17:21:32 -0300

    primera versao do shut

---
+++ src/shut_v3/pkg0/Seu.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shut_v3.pkg0;
+
+/**
+ *
+ * @author will
+ */
+class Seu {
+    
+}
